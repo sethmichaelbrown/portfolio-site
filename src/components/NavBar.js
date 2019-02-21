960bf183f49b2950ac68f8ac03d67111d997a652
SMB <sethmichaelbrown@gmail.com>
2019-02-21 14:59:48 -0700

    App set up + NavBar component created

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+// Packages
+import React, { Component } from 'react'
+
+// Components
+
+// Styling
+import '../App.css'
+import '../Fonts.css'
+
+const NavBar = (props) => {
+
+  return (
+    <div className="NavBar">
+      <h1>Hello from NavBar</h1>
+
+    </div>
+  )
+}
+
+export default NavBar

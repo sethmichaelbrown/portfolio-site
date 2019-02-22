46ba5ac44e0cc57eb0bd00034613b3eece3518fa
SMB <sethmichaelbrown@gmail.com>
2019-02-22 14:49:11 -0700

    responsive design to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,7 +14,7 @@ import '../Fonts.css'
 
 const Contact = (props) => {
 
-  const bio = `My name is Seth Brown, I am a full stack developer with an emphasis on frontend work in ReactJS. I have worked with individual clients looking for a website for their small business, non-profits, and large companies looking to move away from bulky and costly solutions like Wix. Clean aesthetics, ease of use and navigation, powerful capabilities, and clear communication during the build are goals I strive for on every project.` 
+  const bio = `My name is Seth Brown, I am a full stack developer with an emphasis on frontend work in ReactJS. I have worked with individual clients looking for a website for their small business, non-profits, and large companies looking to move away from bulky and costly solutions like Wix. Clean aesthetics, ease of use and navigation, powerful capabilities, and clear communication during the build are goals I strive for on every project.`
 
   return (
     <div className="Contact container">
@@ -24,14 +24,17 @@ const Contact = (props) => {
             <a href="https://www.linkedin.com/in/seth-brown1/" target='_blank'><div className="smb-profile" /></a>
           </div>
         </div>
-        <div className="col-lg-10 col-md-7 col-sm-6 profile-text">
-          <p>{bio}</p>
+        <div className="col-lg-10 col-md-7 col-sm-6 profile-text my-auto">
+          <div className="row contact-header">
+            <h1>Get In Touch</h1>
+          </div>
+
         </div>
       </div>
 
 
       <div className="row"></div>
-       
+
 
 
     </div >

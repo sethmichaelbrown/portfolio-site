9f6b4c0185045e1d5859794f9deb85a200563616
SMB <sethmichaelbrown@gmail.com>
2019-02-22 09:41:23 -0700

    Dynamically rendering cards for projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,6 +6,7 @@ import { Route, Link } from 'react-router-dom'
 
 import bts from './project-images/bts-app.png'
 import riverfront from './project-images/riverfront-hearing.png'
+import rjmusic from './project-images/rjmusic.png'
 
 // Styling
 import '../App.css'
@@ -16,7 +17,7 @@ const Portfolio = (props) => {
   const projects = [
     {
       title: 'Bus To Show',
-      description: "Member of a team that built, deployed, and continue to maintain the new Bus To Show website that allows for reservations to be fulfilled \n Lead the frontend development & UI / UX, assisted with backend architecture",
+      description: "Member of a team that built, deployed, and continue to maintain the new Bus To Show website that allows for reservations to be fulfilled. Lead the frontend development & UI / UX, assisted with backend architecture.",
       link: '',
       image: bts,
       githubLink: 'https://github.com/sethmichaelbrown/bus-to-show-2',
@@ -26,14 +27,15 @@ const Portfolio = (props) => {
       title: 'Riverfront Hearing',
       description: "Michigan’s leading family audiologist practice was looking to move away from using Wix and have a custom site, with ease of navigation and simplicity of design taking priority.",
       image: riverfront,
-      link: 'riverfront.surge.sh',
+      link: 'http://riverfront.surge.sh',
       githubLink: 'https://github.com/sethmichaelbrown/riverfront-website',
       tools: 'JavaScript, MaterializeCSS, HTML5+ CSS3, Node.js'
     },
     {
       title: 'Robert James Music',
       description: "Boulder-based musician and guitar teacher, Robb James, needed a rebuild of his website to better facilitate bringing in new clients.",
-      link: 'rj-music.surge.sh',
+      image: rjmusic,
+      link: 'http://rj-music.surge.sh',
       githubLink: 'https://github.com/sethmichaelbrown/robert-james-music',
       tools: 'JavaScript, MaterializeCSS, HTML5+ CSS3, Node.js'
     },
@@ -53,13 +55,17 @@ const Portfolio = (props) => {
       <div className="row">
         {projects.map(project => {
           return (
-            <div className="col-md-6 col-sm-12 d-flex">
-              <div class="card flex-fill">
+            <div className="col-md-6 col-sm-12 d-flex ">
+              <div class="card flex-fill mb-2">
                 <img src={project.image} class="card-img-top card-image" alt={project.title} />
                 <div class="card-body">
                   <h5 class="card-title">{project.title}</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                  <p class="card-text card-description">{project.description}</p>
+                  {project.link ? 
+                  <a href={project.link} class="btn mr-2">See Site</a> :
+                  <a href={project.link} disabled class="btn mr-2 disabled">In Progress</a>
+                  }
+                  <a href={project.githubLink} class="btn ">See Code</a>
                 </div>
               </div>
             </div>
